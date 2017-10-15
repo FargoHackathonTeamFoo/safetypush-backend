6febf00fb327dee19a74560a54ccfaa41fbe81a2
--global <--global>
2017-10-14 19:53:03 -0500

    created database schema

---
+++ setup/setup.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE safetypush1
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+\connect safetypush1;
+
+CREATE EXTENSION Postgis;
+
+CREATE TABLE public.channel
+(
+    id serial NOT NULL,
+    name text,
+    CONSTRAINT id PRIMARY KEY (id)
+);
+
+CREATE TABLE public.subscriber
+(
+    id serial NOT NULL,
+    location geography,
+    webpush jsonb,
+    CONSTRAINT subscriber_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE public.message
+(
+    id serial NOT NULL,
+    affected geometry,
+    channel integer,
+    title text,
+    push_body text,
+    body text,
+    geojson jsonb,
+    expired boolean,
+    CONSTRAINT message_pkey PRIMARY KEY (id),
+    CONSTRAINT channel_fk FOREIGN KEY (channel)
+        REFERENCES public.channel (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
